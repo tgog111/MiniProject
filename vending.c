911349ab721ae855282c86179114b9a3b25ab08c
mingseobi <22200078@handong.ac.kr>
2023-05-17 14:01:04 +0900

    Updated vending.c

--- vending.c
+++ vending.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include "vending.h"
 
-
 int menu()
 {
     int menu;
@@ -25,26 +24,63 @@ int menu()
     return  menu;
 } // 메뉴
 
-int add_item(Item *item, int count); // 물건 추가 함수
 
-void print_list(Item *item, int count); // 조회 함수
+int add_item(Item *item, int count) // 물건 추가 함수
+{
 
-void updated_item(Item *item, int count); // 수정 함수
+} 
 
-int delete_item(Item *item, int count); // 물건 삭제 함수
+void print_list(Item *item, int count) // 조회 함수
+{
 
-void save_file(Item *item, int count, char filename[100]); // 파일 저장 함수
+} 
 
-int load_file(Item *item, char filename[100]); // 파일에서 읽어오는 함수
+void updated_item(Item *item, int count) // 수정 함수
+{
 
-void input_money(Item *item, int count); // 현금을 자판기에 투입하는 함수
+} 
 
-void buy_item(Item *item, int count); // 물건을 구매하는 함수
+int delete_item(Item *item, int count) // 물건 삭제 함수
+{
 
-void math_reward(Item *item, int count); // 포인트 계산 함수
+}
 
-void check_money(Item *item, int count); // 투입된 금액을 확인하는 함수
+void save_file(Item *item, int count, char filename[100]) // 파일 저장 함수
+{
 
-void check_point(Item *item, int count); // 현재 보유한 포인트 잔액 확인 기능
+}
 
-void exchange_money(Item *item, int count); // 거스름돈 함수
+int load_file(Item *item, char filename[100]) // 파일에서 읽어오는 함수
+{
+
+}
+
+void input_money(Item *item, int count) // 현금을 자판기에 투입하는 함수
+{
+
+}
+
+void buy_item(Item *item, int count) // 물건을 구매하는 함수
+{
+
+}
+
+void math_reward(Item *item, int count) // 포인트 계산 함수
+{
+
+}
+
+void check_money(Item *item, int count) // 투입된 금액을 확인하는 함수
+{
+
+}
+
+void check_point(Item *item, int count) // 현재 보유한 포인트 잔액 확인 기능
+{
+
+}
+
+void exchange_money(Item *item, int count) // 거스름돈 함수
+{
+
+}
